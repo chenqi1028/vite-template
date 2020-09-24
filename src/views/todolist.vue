588dc26ba3a9744b2c88efc0ba87b0a0b797990c
chenqi <chenqi@custouch.com>
2020-09-24 18:51:47 +0800

    初始化

---
+++ src/views/todolist.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item,index) in proc.inProc" :key="item.id" @click="proc.remove(index)">{{item.msg}}</li>
+    </ul>
+    <input type="text" v-model="proc.newProc.msg">
+    <button @click="proc.add">添加</button>
+  </div>
+</template>
+
+<script>
+import { ref,reactive } from 'vue';
+export default {
+  name: 'todolist',
+  setup() {
+    let { proc } = useProcess();
+    return {proc};
+  }
+}
+function useProcess(){
+  let proc = reactive({
+    newProc:{msg:""},
+    inProc:[
+      {msg:'abc1'},
+      {msg:'abc2'},
+      {msg:'abc3'}
+    ],
+    remove(index){
+      this.inProc = this.inProc.filter((newProc,idx) => idx != index);
+    },
+    add(){
+      let newProc = Object.assign({},this.newProc)
+      this.inProc.push(newProc)
+      this.newProc.msg = ""
+    }
+  })
+  return {proc};
+}
+</script>
+<style scoped>
+
+</style>                  
